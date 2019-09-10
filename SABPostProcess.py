5530adaaf25c5080d425d5533f0ee2fbcefb4408
Corey-Evans <corey.evans@gmail.com>
2019-09-10 11:33:06 +1000

    Update SABPostProcess.py

--- SABPostProcess.py
+++ SABPostProcess.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python
-
+#!/usr/bin/python
 import os
+import json
 import sys
+from shutil import copytree, rmtree
 from autoprocess import autoProcessTV, autoProcessMovie, autoProcessTVSR, sonarr, radarr
 from readSettings import ReadSettings
 from mkvtomp4 import MkvtoMp4
 import logging
 from logging.config import fileConfig
+import fnmatch
+from os.path import isdir, join
 
 logpath = '/var/log/sickbeard_mp4_automator'
 if os.name == 'nt':
@@ -36,7 +39,14 @@ if len(sys.argv) < 8:
 # 6 Group that the NZB was posted in e.g. alt.binaries.x
 # 7 Status of post processing. 0 = OK, 1=failed verification, 2=failed unpack, 3=1+2
 
-settings = ReadSettings(os.path.dirname(sys.argv[0]), "autoProcess.ini")
+# Chcanged by me, use custom ini based on category
+ini_file = "autoProcess-{}.ini".format(str(sys.argv[5]).lower())
+log.info(ini_file)
+settings = ReadSettings(
+    os.path.dirname(sys.argv[0]),
+    ini_file,
+)
+
 categories = [settings.SAB['sb'], settings.SAB['cp'], settings.SAB['sonarr'], settings.SAB['radarr'], settings.SAB['sr'], settings.SAB['bypass']]
 category = str(sys.argv[5]).lower()
 path = str(sys.argv[1])
@@ -78,6 +88,28 @@ if settings.SAB['convert']:
         path = converter.output_dir
 else:
     log.info("Passing without conversion.")
+
+# Added by me, copy subs before sending to Sonarr/Radarr - because their extra import sucks (deletes some subs and doesn't keep forced tag)
+def include_patterns(*patterns):
+    def _ignore_patterns(path, all_names):
+        keep = (name for pattern in patterns
+                        for name in fnmatch.filter(all_names, pattern))
+        dir_names = (name for name in all_names if isdir(join(path, name)))
+        return set(all_names) - set(keep) - set(dir_names)
+    return _ignore_patterns
+org_path = str(sys.argv[1])
+copy_path = org_path.replace('/downloads', '/subs_to_import')
+# if already exists, remove old
+log.info("Checking if {} already exists.".format(copy_path))
+if os.path.isdir(copy_path):
+    log.info("Removing {}.".format(copy_path))
+    rmtree(copy_path, ignore_errors=True)
+else:
+    log.info("Doesn't already exist: {}.".format(copy_path))
+copytree(org_path, copy_path, ignore=include_patterns('*.sub', '*.smi', '*.ssa', '*.ass', '*.vtt', '*.srt', '*.idx'))
+if os.path.isdir(copy_path) and not os.listdir(copy_path):
+    # If no subs found, no point having dir
+    os.rmdir(copy_path)
 
 # Send to Sickbeard
 if (category == categories[0]):
